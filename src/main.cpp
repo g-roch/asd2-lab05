130bb8f8c5f915b21f715f6e8f8abeb9098640e2
Ciol Hcor <ciol@hcor.ch>
2020-01-10 15:42:56 +0100

    Ajout fonction de nettoyage des mots

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,31 @@
 
 #include <iostream>
+#include <string>
 
 using namespace std;
 
+/**
+ * Converti le mot en minuscule et ne garde que les caractères alphabétique
+ * et les apostrophes à l'intérieur du mot
+ * @param str Chaine à néttoyer
+ */
+void cleanWord(string & str) {
+    string newstr;
+    for(size_t i = 0; i < str.size(); ++i) {
+        if(isalpha(str[i]) or str[i] == '\'')
+            newstr.push_back(tolower(str[i]));
+    }
+    while(newstr.front() == '\'')
+        newstr.erase(0, 1);
+    while(newstr.back() == '\'')
+        newstr.erase(newstr.size() - 1);
+    swap(newstr, str);
+}
+
 int main(int argc, const char * argv[]) {
-	cout << "Hello World !" << endl;
+    string s ("''Hello World ! j'ai''''");
+    cleanWord(s);
+
+	cout << s << endl;
 	return EXIT_SUCCESS;
 }
